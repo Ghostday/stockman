0014b6ca3f8adeb90143dfb3b4cf881ac56e995e
Chris <fablekey@gmail.com>
2022-08-02 17:04:13 +1000

    firebase env variables imported && gapi added to http scope

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      path: "path-browserify"
+    }
+  },
   clearScreen: false,
   server: {
     strictPort: true,
   },
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ['VITE_', 'TAURI_', 'FIREBASE_'],
   build: {
     target: ['es2021', 'chrome100', 'safari13'],
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
