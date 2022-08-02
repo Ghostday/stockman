4201686dbb7e94b3b5c3d631b0c48c0a176374f9
Chris <fablekey@gmail.com>
2022-08-03 02:10:55 +1000

    now loads products into state

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Product, Document, ParsedProduct } from './types';
+import { getDocs } from './lib/calls'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState<undefined | Document<ParsedProduct>[]>();
 
+  useEffect(() => {
+    getDocs("products").then(data => setProducts(data));
+  },[]);
+
+  console.log(products);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {products && products.map(product => <p>{product.fields.price}</p>)}
     </div>
   )
 }
