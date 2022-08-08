c94755f84a546b7eca18e6daf8b036fcb3184363
Chris <fablekey@gmail.com>
2022-08-08 20:38:20 +1000

    stockpage now displays baseline products and auth

--- src/pages/StockPage.tsx
+++ src/pages/StockPage.tsx
@@ -1,6 +1,19 @@
-export default function StockPage() {
+import { Auth, ParsedProduct, Document } from '../types'
+import { useEffect, useState } from 'react'
+import { getDocs } from '../lib/calls';
+
+export default function StockPage({user}: {user: Auth}) {
+  const [products, setProducts] = useState<undefined | Document<ParsedProduct>[]>();
+
+  useEffect(() => {
+    getDocs("products").then(data => setProducts(data));
+  }, [user]);
+
 
   return (
-    <h1>Stock Page</h1>
+    <div>
+      <h1>Welcome {user.name}</h1>
+      {products && products.map(product => <p>{product.fields.name}</p>)}
+    </div>
   )
 }
